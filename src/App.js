2d21d5f830802b8f9be95081920757065b51b063
Hamdan Salih <hamdansalihpc@gmail.com>
2022-12-13 14:26:29 +0530

    styling done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css';
 import Banner from './components/Banner/banner';
 import Navbar from './components/navbar';
+import Rowpost from './components/RowPost/rowpost';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Banner/>
+      <Rowpost/>
     </div>
   );
 }
