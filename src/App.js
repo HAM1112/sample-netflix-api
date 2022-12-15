0be23ebb27e0cd8787d7514c266205c276b06ef0
Hamdan Salih <hamdansalihpc@gmail.com>
2022-12-15 14:22:50 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import './App.css';
 import Banner from './components/Banner/banner';
-import Navbar from './components/navbar';
+import Navbar from './components/navbar/navbar';
 import Rowpost from './components/RowPost/rowpost';
+import {originals,action } from './urls'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Banner/>
-      <Rowpost/>
+      <Rowpost url={originals} title='Netflix Originals'  />
+      <Rowpost url={action} title='Action' isSmall />
+      <Rowpost url={action} title='Action' isSmall />
+      <Rowpost url={action} title='Action' isSmall />
+      <Rowpost url={action} title='Action' isSmall />
     </div>
   );
 }
--- src/components/Banner/banner.js
+++ src/components/Banner/banner.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from '../../axios'
+import { useEffect } from 'react';
+import { API_KEY , imageUrl } from '../../constansts/constants';
 import "./banner.css"
 
+
 function Banner() {
+  const [movie, setMovie] = useState();
+
+  useEffect(() => {
+    axios.get(`trending/all/week?api_key=${API_KEY}&language=en-US`).then((response)=>{
+      console.log(response.data);
+      setMovie(response.data.results[Math.floor(Math.random() * 19) + 1])
+      //setMovie(response.data.results[0])
+    })
+  }, []);
   return (
-    <div className='banner'>
+    <div 
+    style={{backgroundImage: `url(${movie? imageUrl + movie.backdrop_path:""})`}}
+    className='banner'>
         <div className='content'>
-            <h1 className='title'>Movie name</h1>
+            <h1 className='title'>{movie ? (movie.title ? movie.title : movie.name) : " " }</h1>
             <div className='banner_button'>
                 <button className='button'>Play</button>
                 <button className='button'>My List</button>
             </div>
-            <h1 className='description'> Lorsed to demonstrate the visual form of a document or a typeface without relying on meaningful content.</h1>
+            <h1 className='description'> {movie ? movie.overview : " "} </h1>
         </div>
         <div className='fade_bottom'></div>
     </div>
